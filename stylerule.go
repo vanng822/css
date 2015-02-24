2c754d7ca22295368a9879eb20d4a186702e669e
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-02-24 20:07:11 +0100

    Adding Text() for style rule

--- stylerule.go
+++ stylerule.go
@@ -1,8 +1,27 @@
 package css
 
-import ()
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 type CSSStyleRule struct {
 	SelectorText string
 	Styles       map[string]*CSSStyleDeclaration
+}
+
+func (sr *CSSStyleRule) Text() string {
+	decls := make([]string, 0, len(sr.Styles))
+	keys := make([]string, 0, len(sr.Styles))
+	for key, _ := range sr.Styles {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		decls = append(decls, sr.Styles[key].Text())
+	}
+
+	return fmt.Sprintf("%s {\n%s\n}\n", sr.SelectorText, strings.Join(decls, ";\n"))
 }
