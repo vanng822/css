2e09c5d72ec6b172268d7b72155d47656bbcc975
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-02 22:05:55 +0100

    More methodic parsing, one step closer to syntax

--- parser.go
+++ parser.go
@@ -48,6 +48,18 @@ func resetContextStyleRule(context *parserContext) {
 	context.State = STATE_NONE
 }
 
+func parseRule(context *parserContext, s *scanner.Scanner, css *CSSStyleSheet) {
+	context.CurrentRule = NewRule(context.NowRuleType)
+	context.NowSelectorText += parseSelector(s)
+	context.CurrentRule.Style.SelectorText = strings.TrimSpace(context.NowSelectorText)
+	context.CurrentRule.Style.Styles = parseBlock(s)
+	if context.CurrentMediaRule != nil {
+		context.CurrentMediaRule.Rules = append(context.CurrentMediaRule.Rules, context.CurrentRule)
+	} else {
+		css.CssRuleList = append(css.CssRuleList, context.CurrentRule)
+	}
+}
+
 // Parse takes a string of valid css rules, stylesheet,
 // and parses it. Be aware this function has poor error handling
 // so you should have valid syntax in your css
@@ -73,8 +85,15 @@ func Parse(csstext string) *CSSStyleSheet {
 		}
 
 		switch token.Type {
+		case scanner.TokenCDO:
+			break
+		case scanner.TokenCDC:
+			break
+		case scanner.TokenComment:
+			break
+		case scanner.TokenS:
+			break
 		case scanner.TokenAtKeyword:
-			context.State = STATE_SELECTOR
 			switch token.Value {
 			case "@media":
 				context.NowRuleType = MEDIA_RULE
@@ -82,6 +101,7 @@ func Parse(csstext string) *CSSStyleSheet {
 				// Parse as normal rule, would be nice to parse according to syntax
 				// https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face
 				context.NowRuleType = FONT_FACE_RULE
+				parseRule(context, s, css)
 			case "@import":
 				// No validation
 				// https://developer.mozilla.org/en-US/docs/Web/CSS/@import
@@ -101,32 +121,11 @@ func Parse(csstext string) *CSSStyleSheet {
 
 			case "@page":
 				context.NowRuleType = PAGE_RULE
+				parseRule(context, s, css)
 			default:
 				panic(fmt.Sprintf("At rule '%s' is not supported", token.Value))
 			}
-
-		case scanner.TokenString:
-			if context.State == STATE_SELECTOR {
-				context.NowSelectorText += token.Value
-				break
-			}
-		case scanner.TokenIdent:
-
-			if context.State == STATE_NONE || context.State == STATE_SELECTOR {
-				context.State = STATE_SELECTOR
-				context.NowSelectorText += strings.TrimSpace(token.Value)
-				break
-			}
-
-		case scanner.TokenDimension:
-			fallthrough
-		case scanner.TokenS:
-			if context.State == STATE_SELECTOR {
-				context.NowSelectorText += token.Value
-				break
-			}
-
-		case scanner.TokenChar:
+		default:
 			if context.State == STATE_NONE {
 				if token.Value == "}" && context.CurrentMediaRule != nil {
 					// close media rule
@@ -134,77 +133,19 @@ func Parse(csstext string) *CSSStyleSheet {
 					context.CurrentMediaRule = nil
 					break
 				}
-				if token.Value != "{" {
-					context.State = STATE_SELECTOR
-					context.NowSelectorText += token.Value
-					break
-				}
-			}
-			if context.State == STATE_SELECTOR {
-				if "{" == token.Value {
-					if context.NowRuleType == MEDIA_RULE {
-						context.CurrentMediaRule = NewRule(context.NowRuleType)
-						context.CurrentMediaRule.Style.SelectorText = strings.TrimSpace(context.NowSelectorText)
-						resetContextStyleRule(context)
-						break
-					} else {
-						context.CurrentRule = NewRule(context.NowRuleType)
-						context.CurrentRule.Style.SelectorText = strings.TrimSpace(context.NowSelectorText)
-						context.CurrentRule.Style.Styles = parseBlock(s)
-						if context.CurrentMediaRule != nil {
-							context.CurrentMediaRule.Rules = append(context.CurrentMediaRule.Rules, context.CurrentRule)
-						} else {
-							css.CssRuleList = append(css.CssRuleList, context.CurrentRule)
-						}
-						resetContextStyleRule(context)
-						break
-					}
-				} else {
-					context.NowSelectorText += token.Value
-				}
-				break
-			}
-		case scanner.TokenPercentage:
-			fallthrough
-		case scanner.TokenHash:
-			if context.State == STATE_NONE || context.State == STATE_SELECTOR {
-				context.State = STATE_SELECTOR
-				context.NowSelectorText += strings.TrimSpace(token.Value)
-				break
 			}
 
-			fallthrough
-		case scanner.TokenNumber:
-			fallthrough
-		case scanner.TokenFunction:
-			fallthrough
-		case scanner.TokenIncludes:
-			fallthrough
-		case scanner.TokenDashMatch:
-			fallthrough
-		case scanner.TokenPrefixMatch:
-			fallthrough
-		case scanner.TokenSuffixMatch:
-			fallthrough
-		case scanner.TokenSubstringMatch:
-			if context.State == STATE_SELECTOR {
+			if context.NowRuleType == MEDIA_RULE {
+				context.CurrentMediaRule = NewRule(context.NowRuleType)
+				context.CurrentMediaRule.Style.SelectorText = strings.TrimSpace(token.Value + parseSelector(s))
+				resetContextStyleRule(context)
+				break
+			} else {
 				context.NowSelectorText += token.Value
+				parseRule(context, s, css)
+				resetContextStyleRule(context)
 				break
 			}
-
-		// Unhandle token
-		case scanner.TokenURI:
-			fallthrough
-		case scanner.TokenUnicodeRange:
-			fallthrough
-		case scanner.TokenCDO:
-			fallthrough
-		case scanner.TokenCDC:
-			fallthrough
-		case scanner.TokenComment:
-			fallthrough
-		default:
-			fmt.Printf("Parse(%d) Unhandled, %s:'%s'\n", context.State, token.Type.String(), token.Value)
 		}
 	}
 	return css
