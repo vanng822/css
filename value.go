2ed7dca59d12cb447c4f42dc15929e81b479941c
Arthur Darcet <arthur@medium.com>
2021-03-02 12:18:26 +0100

    CSSValue.ParsedText method

--- value.go
+++ value.go
@@ -27,7 +27,8 @@ Loop:
 }
 
 func NewCSSValueString(data string) *CSSValue {
-	data = strings.ReplaceAll(data, `"`, `\\"`)
+	data = strings.ReplaceAll(data, `\`, `\\`)
+	data = strings.ReplaceAll(data, `"`, `\"`)
 	data = `"` + data + `"`
 	token := scanner.Token{scanner.TokenString, data, 0, 0}
 	return &CSSValue{Tokens: []*scanner.Token{&token}}
@@ -37,6 +38,24 @@ func (v *CSSValue) Text() string {
 	var b strings.Builder
 	for _, t := range v.Tokens {
 		b.WriteString(t.Value)
+	}
+	return strings.TrimSpace(b.String())
+}
+
+func (v *CSSValue) ParsedText() string {
+	var b strings.Builder
+	for _, t := range v.Tokens {
+		switch t.Type {
+		case scanner.TokenString:
+			val := t.Value[1 : len(t.Value)-1] // remove trailing / leading quotes
+			val = strings.ReplaceAll(val, `\"`, `"`)
+			val = strings.ReplaceAll(val, `\'`, `'`)
+			val = strings.ReplaceAll(val, `\\`, `\`)
+			// \A9 should be replaced by the corresponding rune
+			b.WriteString(val)
+		default:
+			b.WriteString(t.Value)
+		}
 	}
 	return strings.TrimSpace(b.String())
 }
--- value_test.go
+++ value_test.go
@@ -6,10 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCSSValueString(t *testing.T) {
-	val := NewCSSValueString("identifier")
-	assert.Equal(t, val.Text(), `"identifier"`)
+func TestCSSValue(t *testing.T) {
+	for _, c := range []struct{ inp, out string }{
+		{`"identifier"`, `identifier`},
+		{`identifier/path`, `identifier/path`},
+		{`"with \' \" \\ chars"`, `with ' " \ chars`},
+	} {
+		t.Run(c.inp, func(t *testing.T) {
+			val := NewCSSValue(c.inp)
+			assert.Equal(t, val.Text(), c.inp)
+			assert.Equal(t, val.ParsedText(), c.out)
+		})
+	}
+}
 
-	val = NewCSSValueString(`with"char`)
-	assert.Equal(t, val.Text(), `"with\\"char"`)
+func TestNewCSSValueString(t *testing.T) {
+	for _, c := range []struct{ inp, out string }{
+		{`identifier`, `"identifier"`},
+		{`with special ' \ " char`, `"with special ' \\ \" char"`},
+	} {
+		t.Run(c.inp, func(t *testing.T) {
+			val := NewCSSValueString(c.inp)
+			assert.Equal(t, val.Text(), c.out)
+			assert.Equal(t, val.ParsedText(), c.inp)
+		})
+	}
 }
